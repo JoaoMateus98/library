7b6898ec4c5495e540a1d7bf3ba926a84d9466c8
JoaoMateus98 <joaobsu98@gmail.com>
2022-06-06 22:52:17 -0400

    JS-created a button that changes wether you read it or not. All functionality is finished

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const createBook = document.querySelector('#create-book');
 const form = document.querySelector('.new-book-form');
 const bookContainer = document.querySelector('.book-container');
 
-let deleteButtons = [];
 let formData;
 let title;
 let author;
@@ -21,24 +20,36 @@ createBook.addEventListener('click', function () {
   title = formData.get('title');
   author = formData.get('author');
   numOfPages = formData.get('pages');
+  wasRead = formData.get('was-read');
 
-  addBookToLibrary(title, author, numOfPages);
+  wasRead = getWasReadValue(wasRead);
+
+  console.log(wasRead);
+
+  addBookToLibrary(title, author, numOfPages, wasRead);
 
   form.reset();
 });
 
-function addBookToLibrary(title, author, numOfPages) {
-  const newBook = new Book(title, author, numOfPages);
+function getWasReadValue(wasRead) {
+  let read = (wasRead == 'on') ? 'was read' : 'was not read';
+  return read;
+}
+
+function addBookToLibrary(title, author, numOfPages, wasRead) {
+  const newBook = new Book(title, author, numOfPages, wasRead);
   const newBookDiv = newBook.createDiv();
 
   library.push(newBookDiv);
   updateDisplay();
 }
 
-function Book(title, author, numOfPages) {
+function Book(title, author, numOfPages, wasRead) {
   this.title = title;
   this.author = author;
-  this.numOfPages = numOfPages
+  this.numOfPages = numOfPages;
+  this.wasRead = wasRead;
+  this.id = '';
 }
 
 Book.prototype.createDiv = function() {
@@ -46,21 +57,36 @@ Book.prototype.createDiv = function() {
   const titleElement = document.createElement('p');
   const authorElement = document.createElement('p');
   const numOfPagesElement = document.createElement('p');
+  const wasReadButton = document.createElement('button');
   const deleteButton = document.createElement('button');
 
   titleElement.innerHTML = this.title;
   authorElement.innerHTML = this.author;
   numOfPagesElement.innerHTML = this.numOfPages;
+  wasReadButton.innerHTML = this.wasRead;
   deleteButton.innerHTML = 'Delete';
+
+  wasReadButton.addEventListener('click', (e) => {
+    this.id = e.path[1].id;
+    wasReadButton.setAttribute('id', `was-read-${this.id}`);
+    this.id = `was-read-${this.id}`;
+    this.updateWasRead(this.id);
+  });
 
   deleteButton.addEventListener('click', (e) => {
     let index = e.path[1].id;
     deleteBook(index);
-  })
+  });
 
-  newBookDiv.append(titleElement, authorElement, numOfPagesElement, deleteButton);
+  newBookDiv.append(titleElement, authorElement, numOfPagesElement, wasReadButton, deleteButton);
   
   return newBookDiv;
+}
+
+Book.prototype.updateWasRead = function(id) {
+  const currentWasReadButton = document.getElementById(id);
+  this.wasRead = (this.wasRead === 'was read') ? 'was not read' : 'was read';
+  currentWasReadButton.innerHTML = this.wasRead;
 }
 
 function updateDisplay() {
